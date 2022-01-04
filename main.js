7e9b50058a8158d11c94398a3c96a0af61187668
NaitikGoel189 <74708219+NaitikGoel189@users.noreply.github.com>
2022-01-04 15:45:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,6 +13,21 @@ function setup(){
 
 function draw(){
     image(video, 0, 0, 480, 380);
+    if(status != ""){
+        objectDetector.detect(video, gotResult);
+        
+        for(i=0; i<objects.length; i++){
+            document.getElementById("status").innerHTML="Status: Object Detected";
+            document.getElementById("number_of_objects").innerHTML="Number Of Objects Detected Are: "+ objects.length;
+
+            fill("#ff0000");
+            percent = floor(objects[i].confidence*100);
+           text(objects[i].label+" "+percent+"%", objects[i].x+15, objects[i].y+15);
+           noFill();
+           stroke("#ff0000");
+           rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height)
+    }
+  }
 }
 
 function start(){
@@ -26,4 +41,14 @@ function modelLoaded(){
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+
+function gotResult(error, results){
+if(error){
+    console.log(error);
+}
+else{
+    console.log(results);
+    objects = results;
+}
 }
